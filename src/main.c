f872ab5460ba95c0cdd8be499862005c8832af27
Sol455 <sol@glowinthedark.co.uk>
2025-04-04 23:13:10 +0100

    Implemented logging through Seggar RTT

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
 
 #define LED0_NODE DT_ALIAS(led0)
 #define SW0_NODE DT_ALIAS(sw0)
@@ -12,6 +13,8 @@ static const struct gpio_dt_spec sw1= GPIO_DT_SPEC_GET(SW1_NODE, gpios);
 static struct gpio_callback sw0_cb;
 static struct gpio_callback sw1_cb;
 
+LOG_MODULE_REGISTER(main);
+
 void sw0_callback(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
     gpio_pin_toggle_dt(&led0);
@@ -22,7 +25,6 @@ void sw1_callback(const struct device *dev, struct gpio_callback *cb, uint32_t p
     gpio_pin_toggle_dt(&led0);
 }
 
-
 int main(void)
 {
     int ret;
@@ -32,7 +34,7 @@ int main(void)
     ret = device_is_ready(sw1.port);
 
     if (!ret) {
-        printk("LED or Button GPIO port not ready!\n");
+        LOG_WRN("LED or Button GPIO port not ready!\n");
         return 0;
     }
 
@@ -48,9 +50,11 @@ int main(void)
     gpio_init_callback(&sw1_cb, sw1_callback, BIT(sw1.pin));
     gpio_add_callback(sw1.port, &sw1_cb);
 
+    LOG_INF("Configured Buttons & Interrupts");
 
     while (1) {
         k_msleep(200);
+        LOG_INF("Hello");
     }
 }
 
