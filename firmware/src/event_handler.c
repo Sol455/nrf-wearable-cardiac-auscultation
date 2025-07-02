dcfbb33eb86f3f3d26730894ffff239afd22df79
Sol455 <sol@glowinthedark.co.uk>
2025-07-02 15:44:23 +0100

    wrapped event per-event function calls into simple functions, advertise(), connect() and record()

--- firmware/src/event_handler.c
+++ firmware/src/event_handler.c
@@ -49,33 +49,63 @@ void _send_demo_heartbeat_packet()
     }
 }
 
+//===========================================FSM State function wrappers===================================
+void _advertise() {
+    led_controller_start_blinking(K_MSEC(500));
+    ble_advertise();
+    app_state = STATE_ADVERTISING;
+}
+
+void _connect() {
+    led_controller_stop_blinking();
+    led_controller_on();
+    //led_set_mode(LED_OFF);
+    app_state = STATE_CONNECTED;
+}
+
+int _record() {
+    led_controller_start_blinking(K_MSEC(150));
+    static char filename[MAX_FILENAME_LEN];
+    generate_filename(filename, sizeof(filename));
+    audio_stream_ptr->wav_config.file_name = filename;
+    int ret = open_wav_for_write(&audio_stream_ptr->wav_config);
+    capture_audio(audio_stream_ptr);
+    return ret;
+}
+
+#if !IS_ENABLED(CONFIG_HEART_PATCH_DSP_MODE)
+void _transit_audio_ble() {
+    //Send Via BLE
+    const int16_t *buf = get_audio_buffer();
+    size_t len_samples = get_audio_buffer_length();
+
+    if (buf && len_samples > 0) {
+        size_t len_bytes = len_samples * sizeof(int16_t);
+        int ret = transmit_audio_buffer((const uint8_t *)buf, len_bytes);
+        if (ret) {
+            LOG_ERR("Failed to transmit audio buffer: %d", err);
+        } else {
+            LOG_INF("Audio buffer transmitted successfully.");
+        }
+        } else {
+        LOG_WRN("No audio buffer available to transmit.");
+    }
+}
+#endif
+//========================================================================================================
+
 static void handle_event(AppEvent evt)
 {
     int ret;
     switch (app_state) {
         case STATE_IDLE:
             if (evt.type == EVENT_BUTTON_0_PRESS && audio_stream_ptr != NULL) {
-                led_controller_start_blinking(K_MSEC(500));
-                ble_advertise();
-                app_state = STATE_ADVERTISING;
+                _advertise();
             }
             break;
-            // if (evt.type == EVENT_BUTTON_0_PRESS && audio_stream_ptr != NULL) {
-            //     led_controller_stop_blinking();
-            //     led_controller_on();
-            //     int ret = open_wav_for_write(&audio_stream_ptr->wav_config);
-            //     capture_audio(audio_stream_ptr);
-            //     led_controller_off();
-            //     //ble_start_advertising();
-            //     app_state = STATE_STREAMING;
-            // }
-            // break;
         case STATE_ADVERTISING:
             if (evt.type == EVENT_BLE_CONNECTED) {
-                led_controller_stop_blinking();
-                led_controller_on();
-                //led_set_mode(LED_OFF);
-                app_state = STATE_CONNECTED;
+                _connect();
             }
             break;
 
@@ -88,32 +118,11 @@ static void handle_event(AppEvent evt)
             }
             //Record Audio to SD Card and then stream over BLE
             if (evt.type == EVENT_BLE_RECORD) {
-                //Record Audio
-                led_controller_start_blinking(K_MSEC(150));
-                static char filename[MAX_FILENAME_LEN];
-                generate_filename(filename, sizeof(filename));
-                audio_stream_ptr->wav_config.file_name = filename;
-                ret = open_wav_for_write(&audio_stream_ptr->wav_config);
-                capture_audio(audio_stream_ptr);
+                ret = _record();
 
                 #if !IS_ENABLED(CONFIG_HEART_PATCH_DSP_MODE)
-                //Send Via BLE
-                const int16_t *buf = get_audio_buffer();
-                size_t len_samples = get_audio_buffer_length();
-
-                if (buf && len_samples > 0) {
-                    size_t len_bytes = len_samples * sizeof(int16_t);
-                    int err = transmit_audio_buffer((const uint8_t *)buf, len_bytes);
-                    if (err) {
-                        LOG_ERR("Failed to transmit audio buffer: %d", err);
-                    } else {
-                        LOG_INF("Audio buffer transmitted successfully.");
-                    }
-                } else {
-                    LOG_WRN("No audio buffer available to transmit.");
-                }
+                _tramsit_audio_ble();
                 #endif
-
                 led_controller_stop_blinking();
                 led_controller_on();
             }
